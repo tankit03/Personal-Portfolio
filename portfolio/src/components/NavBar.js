42e9d36a89f27b4e845f59a4b37abbba17bbf8fc
tankit03 <huparetanish@gmail.com>
2023-12-27 01:48:01 -0800

    added animations - Tanish Hupare

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -3,20 +3,28 @@ import './NavBar.css';
 import  Logo  from "../assets/tan-logo-b.png";
 import Contact from "../assets/Contact-1.png";
 import { Link } from "react-scroll";
+import { motion } from "framer-motion";
+import { navLinksAnimation } from "./animationSettings";
 
 const NavBar = () => {
     
     return (
+       
         <nav className="navbar">
+            
+            <motion.div className="logo-anim" {...navLinksAnimation}>
             <img src={ Logo } alt="logo" className="logo" />
-            <div className="nav-links">
+            </motion.div>
+
+            <motion.div className="nav-links" {...navLinksAnimation}>
                 <Link className="linkMenuItem" >Home</Link>
                 <Link className="linkMenuItem" >About</Link>
                 <Link className="linkMenuItem" >Project</Link>
                 <Link className="linkMenuItem" >Photography</Link>
-            </div>
-            <button className="Contact-btn">
-                <img src={ Contact } alt="Contact" className="Contact-img" />Contact Me</button>
+            </motion.div>
+            <motion.div className="contact-links" {...navLinksAnimation}>
+                <button className="Contact-btn"> <img src={ Contact } alt="Contact" className="Contact-img" />Contact Me</button>
+            </motion.div>
         </nav>
         
     );
---
+++ portfolio/src/components/animationSettings.js
@@ -0,0 +1,7 @@
+import { motion } from "framer-motion";
+
+export const navLinksAnimation = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    transition: { delay: 0.5, duration: 1.5 }
+};
