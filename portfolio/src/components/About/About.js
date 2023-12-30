345d07a4b7d5bf23ea327e6b658a1be93fccf42a
tankit03 <huparetanish@gmail.com>
2023-12-29 16:36:01 -0800

    Have more images of my self for about page - Tanish H

--- portfolio/src/components/About/About.js
+++ portfolio/src/components/About/About.js
@@ -1,15 +1,47 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./About.css";
 import { motion, useAnimation  } from "framer-motion";
+import { useInView } from 'react-intersection-observer';
 import portrait1 from "../assets/portrait1.jpg";
+import portrait2 from "../assets/portrait2.jpg";
+import portrait3 from "../assets/portrait3.jpg";
+
+const portraits = [portrait1, portrait2, portrait3];
 
 const About = () => {
+
+    const controls = useAnimation();
+    const { ref, inView } = useInView({
+        threshold: 0.45,
+    });
+
+    const [currentPortrait, setCurrentPortrait] = useState(0);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCurrentPortrait((currentPortrait + 1) % portraits.length);
+        }, 3000); // Change portrait every 5 seconds
+        return () => clearInterval(timer); // Clean up timer
+    }, [currentPortrait]);
+
+    React.useEffect(() => {
+        if (inView) {
+            controls.start({ scale: .9, transition: { duration: 0.5 } });
+        } else {
+            controls.start({ scale: .8, transition: { duration: 0.5 } });
+        }
+    }, [controls, inView]);
+
     return(
 
-        <div>
-            <div className="planet-center-1"/>
+        <div ref={ref}>
+
+            <motion.div className="planet-center-1"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 5 }}/>
             <div className="planet-center-2"/>
-            <section id="About">
+            <motion.section id="About" animate={controls}>
                 
                 
                 
@@ -23,16 +55,16 @@ const About = () => {
                     Outside the work, I immerse myself in a world of hobbies that balance my academic pursuits. 📷 Photography allows me to capture the beauty of fleeting moments, while my love for traveling ✈️ takes me to new destinations, enriching my perspective of different cultures. In the kitchen, I enjoy the art of cooking 🍳, experimenting with flavors to create culinary delights. My adventurous spirit is further fueled by rock climbing 🧗, a hobby that challenges me physically and mentally, pushing my limits amidst nature's grandeur.
                     </span>
                     <motion.img 
-                        src={portrait1} 
+                        src={portraits[currentPortrait]} 
                         alt="potrait" 
                         className="About-img"
-                        whileHover={{ scale: 1.1 }} // Add this line
+                        whileHover={{ scale: 1.05 }} // Add this line
                     />
                     
                 </div>
                 
 
-            </section>
+            </motion.section>
         </div>
 
 
