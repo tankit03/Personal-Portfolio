a13b52af02292343a877a87bae3687dc53996a0a
tankit03 <huparetanish@gmail.com>
2024-01-08 19:53:40 -0800

    added sandwich button navbar

--- portfolio/src/components/NavBar/NavBar.js
+++ portfolio/src/components/NavBar/NavBar.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import './NavBar.css';
 
 import resume from "../assets/resume-new.pdf";
-import  Logo  from "../assets/tan-logo-b.png";
+
 import Contact from "../assets/Contact-1.png";
+import sandWhichIcon from "../assets/icons/menu-icon.png";
+import Close from "../assets/icons/close-icon.png";
+
 import { Link, scroller } from "react-scroll";
 import { motion } from "framer-motion";
 import { navLinksAnimation } from "../animationSettings";
 
 const NavBar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
 
     const scrollToElement = (elementId) => {
         setTimeout(() => {
@@ -19,20 +24,27 @@ const NavBar = () => {
             });
         }, 1); // Adjust delay as needed
     };
+
+    const handleToggle = () => {
+        setIsOpen(!isOpen);
+    };
     
     return (
        
         <nav className="navbar">
             
-            <motion.div className="logo-anim" {...navLinksAnimation}>
-            <img src={ Logo } alt="logo" className="logo" />
-            </motion.div>
+            
 
-            <motion.div className="nav-links" {...navLinksAnimation}>
-                <Link onClick={() => scrollToElement('Home')} className="linkMenuItem">Home</Link>
-                <Link onClick={() => scrollToElement('About')} className="linkMenuItem">About</Link>
-                <Link onClick={() => scrollToElement('Projects')} className="linkMenuItem" >Project</Link>
-                <Link onClick={() => scrollToElement('Photography')} className="linkMenuItem" >Photography</Link>
+            <div onClick={handleToggle} className="menu-icon">
+            < img src = { sandWhichIcon} alt = "sandWhichIcon" className="SandIcon"/>
+            </div>
+
+            <motion.div className={`nav-links ${isOpen ? "open" : ""}`} {...navLinksAnimation}>
+                <img src={Close} alt="Close" onClick={handleToggle} className="close-icon" />
+                <Link onClick={() => {scrollToElement('Home'); handleToggle();}} className="linkMenuItem">Home</Link>
+                <Link onClick={() => {scrollToElement('About'); handleToggle();}} className="linkMenuItem">About</Link>
+                <Link onClick={() => {scrollToElement('Projects'); handleToggle();}} className="linkMenuItem" >Project</Link>
+                <Link onClick={() => {scrollToElement('Photography'); handleToggle();}} className="linkMenuItem" >Photography</Link>
                 <a href={resume} target="_blank" className="linkMenuItem">Resume</a>
             </motion.div>
             <motion.div className="contact-links" {...navLinksAnimation}>
